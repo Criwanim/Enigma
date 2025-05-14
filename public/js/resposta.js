d5ab79522805f10c706eb5a0d1368148b813c2a2
Criwanim <criwanim@gmail.com>
2025-05-14 14:45:05 -0300

    teste deploy

--- public/js/resposta.js
+++ public/js/resposta.js
@@ -86,7 +86,7 @@ function aplicarEfeitoContador(contador) {
   void contador.offsetWidth;
   contador.style.animation = null;
 
-  // Após 5 segundos, volta ao normal
+  // Após 5 segundos, volta ao normal // teste
   temporizadorContador = setTimeout(() => {
     contador.classList.remove("contador-excedido");
     contador.classList.add("contador-normal");
